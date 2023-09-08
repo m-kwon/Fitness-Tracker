31de46a26dbb84e17efbdaa584639e0c2ae15490
Matthew Kwon <matthew.o.kwon@gmail.com>
2023-09-07 17:20:08 -0700

    Created Exercise schema

---
+++ server/db/Exercise.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const db = require('./index.js');
+const Schema = mongoose.Schema;
+
+const exerciseSchema = new mongoose.Schema({
+  name: String,
+  weight: Number,
+  reps: Number,
+  sets: Number,
+  days: [String],
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  updated: {
+    type: Date,
+    default: Date.now
+  },
+  programId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Program',
+    required: true
+  }
+});
+
+const Exercise = mongoose.model('Exercise', exerciseSchema);
+
+module.exports = Exercise;

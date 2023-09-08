e8fe41bee1b8056c435ee042d35b7f10a04a6910
Matthew Kwon <matthew.o.kwon@gmail.com>
2023-09-07 18:20:38 -0700

    Implemented Exercise component

--- client/src/components/Exercise/Exercise.js
+++ client/src/components/Exercise/Exercise.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
 
-const Exercise = () => {
+import ExerciseItem from './ExerciseItem';
+
+const Exercise = ({match}) => {
+
+  const programId = match.params.programId;
+
+  const dispatch = useDispatch();
+
+  const [exercises, setExercises] = useState([]);
+
+  useEffect(() => {
+    axios.get(`http://localhost:3000/api/exercise/${programId}`)
+      .then(exercises => setExercises(exercises.data))
+      .catch(err => console.log(err));
+  }, []);
+
+  const exerciseList = exercises.map((exercise, index) => (<ExerciseItem key={index} exercise={exercise}/>))
+
   return (
     <div>
       Exercise
+      {exerciseList}
     </div>
   )
 }

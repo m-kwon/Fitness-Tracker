a710b4fb2ac402690b0a3eceec734f18531e6213
Matthew Kwon <matthew.o.kwon@gmail.com>
2023-09-07 18:09:43 -0700

    Created Program component

--- client/src/components/Program/Program.js
+++ client/src/components/Program/Program.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import axios from 'axios';
+
+import { getPrograms } from '../../redux/actions/';
+import CreateProgram from './CreateProgram';
+import ProgramItem from './ProgramItem';
 
 const Program = () => {
+
+  const dispatch = useDispatch();
+
+  const userId = useSelector(state => state.userId);
+  const programs = useSelector(state => state.programs);
+
+  useEffect(() => {
+    axios.get(`http://localhost:3000/program/user/${userId}`)
+      .then(programs => dispatch(getPrograms(programs.data)))
+      .catch(err => console.log(err));
+  }, []);
+
+  const programList = programs.map(program => (<ProgramItem program={program}/>));
+
   return (
     <div>
-      Program
+      <CreateProgram userId={userId}/>
+      {programList}
     </div>
   )
 }

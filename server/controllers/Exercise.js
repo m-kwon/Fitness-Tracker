656ee4d92b62bc562be2a9041b3154f4a3050298
Matthew Kwon <matthew.o.kwon@gmail.com>
2023-09-11 20:09:54 -0700

    Added deleteExercise controller

--- server/controllers/Exercise.js
+++ server/controllers/Exercise.js
@@ -7,13 +7,21 @@ const getExercise = (req, res) => {
     .catch(err => res.status(400).send(err));
 };
 
-const postExercise = async (req, res) => {
+const postExercise = (req, res) => {
   Exercise.create(req.body)
     .then(exercises => res.status(200).send(exercises))
     .catch(err => res.status(400).send(err));
 };
 
+const deleteExercise = (req, res) => {
+  const { exerciseId } = req.params;
+  Exercise.deleteOne({_id: exerciseId})
+    .then(exercise => res.status(200).send(exercise))
+    .catch(err => res.status(400).send(err));
+};
+
 module.exports = {
   getExercise,
-  postExercise
+  postExercise,
+  deleteExercise
 };

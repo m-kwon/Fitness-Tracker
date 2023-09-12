3b98d933105e506270546fed1c46117fde317d72
Matthew Kwon <matthew.o.kwon@gmail.com>
2023-09-11 20:21:53 -0700

    Updated App component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Container } from '@material-ui/core';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
@@ -6,17 +6,21 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './Navbar/Navbar';
 import Program from './Program/Program';
 import Exercise from './Exercise/Exercise';
+import Auth from './Auth/Auth';
 
 const App = () => {
 
   const programId = useSelector(state => state.programId);
+  const [currentId, setCurrentId] = useState(null);
 
   return (
     <Router>
       <Container>
         <Navbar />
         <Switch>
-          <Route path="/" exact component={Program}/>
+          <Route path="/" exact><Program currentId={currentId} setCurrentId={setCurrentId}/></Route>
+          <Route path="/browse" exact><Program currentId={currentId} setCurrentId={setCurrentId}/></Route>
+          <Route path="/auth" exact component={Auth}/>
           <Route path="/program/:programId" component={Exercise}/>
         </Switch>
       </Container>

acc3011dfbf9349915e4855f0a08a7a78cc204a3
Matthew Kwon <matthew.o.kwon@gmail.com>
2023-09-11 20:23:44 -0700

    Added more functionality to Navbar and added some stylization

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,45 +1,55 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import MenuIcon from '@material-ui/icons/Menu';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    paddingBottom: '50px'
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+import { AppBar, Typography, Toolbar, Button, Avatar, makeStyles } from '@material-ui/core';
+import decode from 'jwt-decode';
+import useStyles from './styles.js';
 
 const Navbar = () => {
 
   const classes = useStyles();
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const location = useLocation();
+
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+
+  const logout = () => {
+    dispatch({type: 'LOGOUT'});
+    history.push('/');
+    setUser(null);
+  };
+
+  useEffect(() => {
+    const token = user?.token;
+    if (token) {
+      const decodedToken = decode(token);
+      if (decodedToken.exp * 1000 < new Date().getTime()) {
+        logout();
+      }
+    }
+    setUser(JSON.parse(localStorage.getItem('profile')));
+  }, [location])
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            <Button component={Link} to="/" color="inherit">
-              Fitness Tracker
-            </Button>
-          </Typography>
-          <Button component={Link} to="/" color="inherit">
-            Home
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <AppBar className={classes.appBar} position="static">
+      <div className={classes.brandContainer}>
+        <Typography component={Link} to="/" className={classes.heading} variant="h4" align="center">Fitness Tracker</Typography>
+      </div>
+      <Toolbar className={classes.toolbar}>
+      <Typography component={Link} to="/browse" className={classes.links} variant="h6" align="center">Browse</Typography>
+        {user ? (
+          <div className={classes.profile}>
+            <Avatar className={classes.purple} alt={user?.result?.name} src={user?.result?.imageUrl}>{user?.result?.name.charAt(0)}</Avatar>
+            <Typography className={classes.userName} variant="h6">{user?.result?.name}</Typography>
+            <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
+          </div>
+        ) : (
+          <Button component={Link} to="/auth" variant="contained" color="primary">Login</Button>
+        )}
+      </Toolbar>
+    </AppBar>
   )
 }
 

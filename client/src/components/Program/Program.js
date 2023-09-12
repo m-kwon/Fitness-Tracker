e37893fd659f233afdfd69d3aa84134e7a6ae748
Matthew Kwon <matthew.o.kwon@gmail.com>
2023-09-11 19:32:01 -0700

    Implemented functionality to Program component

--- client/src/components/Program/Program.js
+++ client/src/components/Program/Program.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
 import Table from '@material-ui/core/Table';
@@ -7,9 +7,10 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import { Link } from 'react-router-dom';
+import Link from '@material-ui/core/Link';
+import Button from '@material-ui/core/Button';
 
-import { getPrograms } from '../../redux/actions/';
+import { getPrograms, deleteProgram } from '../../redux/actions/programs';
 import CreateProgram from './CreateProgram';
 import Exercise from '../Exercise/Exercise';
 
@@ -21,29 +22,28 @@ const Program = () => {
   const programs = useSelector(state => state.programs);
 
   useEffect(() => {
-    axios.get(`http://localhost:3000/api/program/user/${userId}`)
-      .then(programs => dispatch(getPrograms(programs.data)))
-      .catch(err => console.log(err));
+    if (userId) {
+      dispatch(getPrograms(userId));
+    }
   }, []);
 
   const programList = programs.map(program => {
     return (
       <TableRow key={program._id}>
         <TableCell component="th" scope="row">
-          <Link to={`/program/${program._id}`}>{program.name}</Link>
+          <Link href={`/program/${program._id}`}>{program.name}</Link>
         </TableCell>
         <TableCell align="right">{program.numOfWeeks}</TableCell>
         <TableCell align="right">{program.created}</TableCell>
         <TableCell align="right">{program.updated}</TableCell>
-        <TableCell align="right">{program.userId}</TableCell>
-        <TableCell align="right">{program._id}</TableCell>
+        <TableCell align="right"><Button color="secondary" onClick={() => {dispatch(deleteProgram(program._id))}}>Delete</Button></TableCell>
       </TableRow>
     )
   });
 
   return (
     <div>
-      <CreateProgram userId={userId}/>
+      <CreateProgram/>
       <TableContainer>
       <Table>
         <TableHead>
@@ -52,8 +52,7 @@ const Program = () => {
             <TableCell align="right">Number of Weeks</TableCell>
             <TableCell align="right">Created</TableCell>
             <TableCell align="right">Updated</TableCell>
-            <TableCell align="right">User ID</TableCell>
-            <TableCell align="right">Program ID</TableCell>
+            <TableCell align="right">Delete</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>

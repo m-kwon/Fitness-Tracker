ed766e070ec14fcb81bce20f7470ee088db0bdcb
Matthew Kwon <matthew.o.kwon@gmail.com>
2023-09-11 20:10:28 -0700

    Added more exercise controllers

--- server/controllers/Exercise.js
+++ server/controllers/Exercise.js
@@ -1,4 +1,5 @@
 const Exercise = require('../db/Exercise');
+const mongoose = require('mongoose');
 
 const getExercise = (req, res) => {
   const { programId } = req.params;
@@ -8,7 +9,8 @@ const getExercise = (req, res) => {
 };
 
 const postExercise = (req, res) => {
-  Exercise.create(req.body)
+  const exercise = req.body;
+  Exercise.create({...exercise, creator: req.userId})
     .then(exercises => res.status(200).send(exercises))
     .catch(err => res.status(400).send(err));
 };
@@ -20,8 +22,20 @@ const deleteExercise = (req, res) => {
     .catch(err => res.status(400).send(err));
 };
 
+const updateExercise = (req, res) => {
+  const { exerciseId } = req.params;
+  const exercise = req.body;
+  if (!mongoose.Types.ObjectId.isValid(exerciseId)) {
+    return res.status(400).send('No program found.');
+  }
+  Exercise.findByIdAndUpdate(exerciseId, {...exercise, _id: exerciseId}, {new: true})
+    .then(exercise => res.status(200).send(exercise))
+    .catch(err => res.status(400).send(err));
+}
+
 module.exports = {
   getExercise,
   postExercise,
-  deleteExercise
+  deleteExercise,
+  updateExercise
 };

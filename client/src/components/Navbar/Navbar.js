d9858f253b672e34438dbc7f5d4d344d49be8cfe
Matthew Kwon <matthew.o.kwon@gmail.com>
2023-09-11 19:19:00 -0700

    Implemented Navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import {Link} from 'react-router-dom';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    paddingBottom: '50px'
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 const Navbar = () => {
+
+  const classes = useStyles();
+
   return (
-    <div>
-      Navbar
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            <Button component={Link} to="/" color="inherit">
+              FitLog
+            </Button>
+          </Typography>
+          <Button component={Link} to="/" color="inherit">
+            Home
+          </Button>
+        </Toolbar>
+      </AppBar>
     </div>
   )
 }

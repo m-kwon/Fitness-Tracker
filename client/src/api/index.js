fb6a0ed8cb4e358f4aaec3d583d8f2c61764cd44
Matthew Kwon <matthew.o.kwon@gmail.com>
2023-09-11 20:20:42 -0700

    Added more API commands

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,11 +1,25 @@
 import axios from 'axios';
 
-const API = axios.create({baseURL: 'http://localhost:3000'});
+const API = axios.create({baseURL: '/api'});
 
-export const getPrograms = (userId) => API.get(`http://localhost:3000/api/program/user/${userId}`);
-export const createProgram = (program) => API.post(`http://localhost:3000/api/program/create`, program);
-export const deleteProgram = (programId) => API.delete(`http://localhost:3000/api/program/${programId}`);
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem('profile')) {
+    req.headers.authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+  }
+  return req;
+});
 
-export const getExercises = (programId) => API.get(`http://localhost:3000/api/exercise/${programId}`);
-export const createExercise = (exercise) => API.post(`http://localhost:3000/api/exercise/create`, exercise);
-export const deleteExercise = (exerciseId) => API.delete(`http://localhost:3000/api/exercise/${exerciseId}`);
+export const getPrograms = () => API.get(`/program/all`);
+export const getProgramsByUser = (userId) => API.get(`/program/user/${userId}`);
+export const getProgramsById = (programId) => API.get(`/program/id/${programId}`);
+export const createProgram = (program) => API.post(`/program/create`, program);
+export const deleteProgram = (programId) => API.delete(`/program/${programId}`);
+export const updateProgram = (programId, program) => API.patch(`/program/${programId}`, program);
+
+export const getExercises = (programId) => API.get(`/exercise/${programId}`);
+export const createExercise = (exercise) => API.post(`/exercise/create`, exercise);
+export const deleteExercise = (exerciseId) => API.delete(`/exercise/${exerciseId}`);
+export const updateExercise = (exerciseId, exercise) => API.patch(`/exercise/${exerciseId}`, exercise);
+
+export const signIn = (formData) => API.post(`/user/signin`, formData);
+export const signUp = (formData) => API.post(`/user/signup`, formData);

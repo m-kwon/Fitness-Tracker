085e1088a0d88cd94923f8c9616af5b2f4ec50fb
Matthew Kwon <matthew.o.kwon@gmail.com>
2023-09-11 19:14:29 -0700

    Formatted and stylized Exercise component with material UI

--- client/src/components/Exercise/Exercise.js
+++ client/src/components/Exercise/Exercise.js
@@ -1,8 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Button from '@material-ui/core/Button';
 
-import ExerciseItem from './ExerciseItem';
+import AddExercise from './AddExercise';
+import { getExercises, deleteExercise } from '../../redux/actions/exercises';
 
 const Exercise = ({match}) => {
 
@@ -10,20 +18,43 @@ const Exercise = ({match}) => {
 
   const dispatch = useDispatch();
 
-  const [exercises, setExercises] = useState([]);
+  const exercises = useSelector(state => state.exercises);
 
   useEffect(() => {
-    axios.get(`http://localhost:3000/api/exercise/${programId}`)
-      .then(exercises => setExercises(exercises.data))
-      .catch(err => console.log(err));
+    dispatch(getExercises(programId));
   }, []);
 
-  const exerciseList = exercises.map((exercise, index) => (<ExerciseItem key={index} exercise={exercise}/>))
+  const exerciseList = exercises.map(exercise => {
+    return (
+      <TableRow key={exercise._id}>
+        <TableCell component="th" scope="row">{exercise.name}</TableCell>
+        <TableCell align="right">{exercise.weight}</TableCell>
+        <TableCell align="right">{exercise.sets}</TableCell>
+        <TableCell align="right">{exercise.reps}</TableCell>
+        <TableCell align="right"><Button color="secondary" onClick={() => {dispatch(deleteExercise(exercise._id))}}>Delete</Button></TableCell>
+      </TableRow>
+    )
+  });
 
   return (
     <div>
-      Exercise
-      {exerciseList}
+      <AddExercise programId={programId} getExercises={getExercises}/>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Exercise Name</TableCell>
+              <TableCell align="right">Weight</TableCell>
+              <TableCell align="right">Sets</TableCell>
+              <TableCell align="right">Reps</TableCell>
+              <TableCell align="right">Delete</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {exerciseList}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   )
 }

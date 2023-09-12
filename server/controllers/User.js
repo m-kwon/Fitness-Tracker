8301612befd3a47139aee40440a4e4e05c07baee
Matthew Kwon <matthew.o.kwon@gmail.com>
2023-09-11 20:11:24 -0700

    Added user controllers with ability to sign up and create an account

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -1,16 +1,45 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../db/User');
 
-const login = (req, res) => {
-  res.send('Login');
+const signin = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const existingUser = await User.findOne({email});
+    if (!existingUser) {
+      return res.status(404).json({message: 'Invalid user'});
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({message: 'Invalid credentials'});
+    }
+    const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, 'test', { expiresIn: '1h' });
+    res.status(200).json({result: existingUser, token});
+  } catch (error) {
+    res.status(500).json({message: 'Something went wrong'});
+  }
 };
 
-const createUser = (req, res) => {
-  User.create(req.body)
-    .then(user => res.status(200).send(user))
-    .catch(err => res.status(400).send(err));
+const signup = async (req, res) => {
+  const { email, password, confirmPassword, firstName, lastName } = req.body;
+  try {
+    const existingUser = await User.findOne({email});
+    if (existingUser) {
+      return res.status(400).json({message: 'User already exists'});
+    }
+    if (password !== confirmPassword) {
+      return res.status(400).json({message: 'Invalid password confirmation'});
+    }
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const result = await User.create({email, password: hashedPassword, name: `${firstName} ${lastName}`});
+    const token = jwt.sign({ email: result.email, id: result._id }, 'test', { expiresIn: '1h' });
+    res.status(200).json({result, token});
+  } catch (error) {
+    res.status(500).json({message: 'Something went wrong'});
+  }
 };
 
 module.exports = {
-  login,
-  createUser
+  signin,
+  signup
 };
